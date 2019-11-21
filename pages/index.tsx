ea3e875c2d6f578bcdd9b6b4a46645d502940576
Marco Ciampini <marcociampini@toaster.co>
2019-11-21 13:10:28 +0800

    feat(home): logo enter animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Home: NextComponentType<{}, PageHomeProps, PageHomeProps> = ({ path, meta,
             <rect x="0" y="0" width="100%" height="100%" fill="url(#dots-grid)"></rect>
           </svg>
 
-          <h1 className="text-primary bg-background z-10 pointer-events-none home-logo-sizing">
+          <h1 className="text-primary bg-background z-10 pointer-events-none home-logo-title">
             <span className="sr-only">{pageTitle}</span>
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
               <g fill="none" fillRule="evenodd">
