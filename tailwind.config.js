44875833838b0ef3b41262d3e1ec91dd02dc1aed
Marco Ciampini <marcociampini@toaster.co>
2019-11-24 15:52:07 +0800

    feat(tailwind): max width theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,11 @@ module.exports = {
       '4/3': [4, 3],
       '21/9': [21, 9],
     },
+    maxWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+    },
   },
   variants: {},
   plugins: [require('tailwindcss-aspect-ratio')()],
