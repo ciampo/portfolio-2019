1a89436469cf359a052ed643cf2a5afb9a6784ba
Marco Ciampini <marcociampini@toaster.co>
2019-12-08 14:05:23 +0800

    fix(media): more precise sizes for content-wide media

--- components/media/sizes-presets.ts
+++ components/media/sizes-presets.ts
@@ -1,13 +1,38 @@
 import { theme } from '../../tailwind.config';
 
+function remToPx(rem: string): number {
+  return parseFloat(rem) * 16;
+}
+
+function pxToRem(px: string): number {
+  return parseFloat(px) / 16;
+}
+
 const sectionPaddingH = theme.spacing['6'];
-const sectionPaddingHPx = parseFloat(sectionPaddingH) * 16;
+const sectionPaddingHPx = remToPx(sectionPaddingH);
 const maxWidthXs = theme.maxWidth.xs;
-const maxWidthXsPx = parseFloat(maxWidthXs) * 16;
+const maxWidthXsPx = remToPx(maxWidthXs);
 
 const content = {
   resolutions: [320, 480, 640, 768, 1024, 1280],
-  sizes: [`calc(100vw - 2 * ${sectionPaddingH})`].join(','),
+  sizes: [
+    `(min-width: ${theme.screens.xl}) calc(${pxToRem(
+      theme.screens.xl
+    )}rem - 2 * ${sectionPaddingH})`,
+    `(min-width: ${theme.screens.lg}) calc(${pxToRem(
+      theme.screens.lg
+    )}rem - 2 * ${sectionPaddingH})`,
+    `(min-width: ${theme.screens.md}) calc(${pxToRem(
+      theme.screens.md
+    )}rem - 2 * ${sectionPaddingH})`,
+    `(min-width: ${theme.screens.sm}) calc(${pxToRem(
+      theme.screens.sm
+    )}rem - 2 * ${sectionPaddingH})`,
+    `(min-width: ${theme.screens.xsm}) calc(${pxToRem(
+      theme.screens.xsm
+    )}rem - 2 * ${sectionPaddingH})`,
+    `calc(100vw - 2 * ${sectionPaddingH})`,
+  ].join(','),
 };
 
 const narrowMedia = {
