86225c68f29592f88c6c6759d056fef6467eb586
Marco Ciampini <marcociampini@toaster.co>
2020-01-07 11:24:55 +0800

    feat(analytics): track click on assets

--- components/utils/RichTextRenderer.tsx
+++ components/utils/RichTextRenderer.tsx
@@ -5,11 +5,23 @@ import {
   Options,
   NodeRenderer,
 } from '@contentful/rich-text-react-renderer';
+import { event } from 'react-ga';
 
 const AssetLink: NodeRenderer = (node, children) => {
   const url: string = node.data ? node.data.target.fields.file.url : null;
+
+  function sendGaEvent(): void {
+    if (window.IS_GA_INIT) {
+      event({
+        category: 'User',
+        action: 'Clicked on asset link',
+        label: url,
+      });
+    }
+  }
+
   return url ? (
-    <a href={url} target="_blank" rel="noopener noreferrer">
+    <a href={url} target="_blank" rel="noopener noreferrer" onPointerDown={sendGaEvent}>
       {children}
     </a>
   ) : null;
