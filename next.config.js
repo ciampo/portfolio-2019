a0cf596f7b367f72f3c218f478c3cdb6662c1eb2
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-16 15:08:42 +0100

    remove custom polyfills script (nextjs 9.3 adds them automatically)

--- next.config.js
+++ next.config.js
@@ -71,18 +71,6 @@ const nextConfig = {
       },
     });
 
-    // From the 'with-polyfills' netx.js example.
-    const originalEntry = config.entry;
-    config.entry = async () => {
-      const entries = await originalEntry();
-
-      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
-        entries['main.js'].unshift('./polyfills.js');
-      }
-
-      return entries;
-    };
-
     return config;
   },
   env: {
